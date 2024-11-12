f1ca005f7d2acefacd3fc8787c99ee282d260d40
vicente.angeles <vicente.angeles@bbva.com>
2024-11-12 11:51:41 -0600

    add main routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,13 +10,6 @@ const routes: Routes = [
   { path: 'activity', loadChildren: () => import('./modules/activity/activity.module').then(m => m.ActivityModule) },
   { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) },
   { path: 'reservation', loadChildren: () => import('./modules/reservation/reservation.module').then(m => m.ReservationModule) },
-  { path: 'splash', loadChildren: () => import('./modules/splash/splash.module').then(m => m.SplashModule) },
-  { path: 'onboarding', loadChildren: () => import('./modules/onboarding/onboarding.module').then(m => m.OnboardingModule) },
-  { path: 'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule) },
-  { path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule) },
-  { path: 'activity', loadChildren: () => import('./modules/activity/activity.module').then(m => m.ActivityModule) },
-  { path: 'profile', loadChildren: () => import('./modules/profile/profile.module').then(m => m.ProfileModule) },
-  { path: 'reservation', loadChildren: () => import('./modules/reservation/reservation.module').then(m => m.ReservationModule) },
   { path: '**', redirectTo: '/splash' }
 ];
 
