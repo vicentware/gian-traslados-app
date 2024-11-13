b72803dfcb533e50fe8a8de218a72296ba91021a
vicente.angeles <vicente.angeles@bbva.com>
2024-11-13 00:40:14 -0600

    add payload splash

--- src/app/modules/splash/splash-routing.module.ts
+++ src/app/modules/splash/splash-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SplashComponent } from './splash.component';
+import { MainComponent } from './pages/main/main.component';
 
-const routes: Routes = [{ path: '', component: SplashComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: SplashComponent,
+    children: [
+      { path: '', redirectTo: 'main', pathMatch: 'full' },
+      { path: 'main', component: MainComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
